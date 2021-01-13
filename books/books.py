79d3b3bdeb7fa4c973edcaa553db685a33379228
Antonia Ritter <rittera@carleton.edu>
2021-01-13 15:28:52 -0600

    Error-catching and beautifying

--- books/books.py
+++ books/books.py
@@ -30,18 +30,21 @@ def parseArguments():
     parser.add_argument('-a', '--author', help = 'Phrase to search for in author names, in quotes if multiple words.')
     parser.add_argument('-t', '--title', help = 'Phrase to search for in title, in quotes if multiple words.')
     parser.add_argument('-y', '--year', help = 'Two full years separated by hyphen or a single year. Ex: 1900-2000 Ex2: 2005')
+    parser.add_argument('extras', nargs=argparse.REMAINDER) # accounts for unexpected syntax, so usage statement prints 
     arguments = parser.parse_args() 
     return arguments 
 
-# searchY is argument.year, year is publication date of book
-# returns True if year is in searchY, or False if not
-# yearMatch(2000-2001, 1900) -> Flase
+
+"""
+    Input: A range or single year (arguments.year) and a publication year 
+    Output: True or False depending on whether the range contains the year 
+"""
 def yearMatch(searchY, year):
-    #user only typed in one year
+    #single year
     if len(searchY)==4:
         y1 = searchY
         y2 = searchY
-    #user entered year range
+    #year range
     else:
         y1 = searchY[:4]
         y2 = searchY[-4:]
@@ -49,6 +52,8 @@ def yearMatch(searchY, year):
         return True
     else:
         return False
+
+
 """
     Input: List of arguments (the output from parseArgument())
     Output: Default dictionary of books matching the arguments
@@ -71,6 +76,7 @@ def multi_arguments(arguments):
 
     #For each book check to see if it matches the arguments and add it to the dictionary if it matches
     for book in bookList:
+
         #Add books only if it matches the author, title, and if in range of publication year
         if (authorBool and titleBool and yearBool and (re.search(arguments.author, book[2], re.IGNORECASE)) 
             and (re.search(arguments.title, book[0], re.IGNORECASE)) and yearMatch(arguments.year, book[1])):
@@ -105,6 +111,12 @@ def multi_arguments(arguments):
     
     return resultsDict
 
+
+"""
+    Input: parsed arguments and a dictionary of books
+    Prints usage statement if there are no arguments
+    Prints books otherwise 
+"""
 def printOutput(arguments, resultDict):
     if arguments.author == None and arguments.title == None and arguments.year == None:
         with open("usage.txt","r") as f:
@@ -124,21 +136,8 @@ def printOutput(arguments, resultDict):
                 for title in resultDict[key]:
                     print("    ", title)
 
+
 def main():
-    filename = 'books.csv'
-
-    # list of lists
-    # each sublist is [title, year, author]
-    bookList = []
-
-    # opening the CSV file 
-    with open(filename, mode ='r') as file:     
-        # reading the CSV file 
-        csvFile = csv.reader(file) 
-        # displaying the contents of the CSV file 
-        for line in csvFile: 
-                bookList.append(line)
-    
     arguments = parseArguments()
     printOutput(arguments, multi_arguments(arguments))
 
