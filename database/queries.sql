8c223d7c6497f2e2f5115fa9f161b0b87dcb2825
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2021-01-23 16:19:36 -0600

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,8 +1,26 @@
+-- Test with the data
+
 SELECT * 
-FROM olympic_committee
+FROM committee
 ORDER BY abbreviation;
 
-SELECT first_name, last_name 
-FROM athlete
-WHERE country = "Kenya"
-ORDER BY last_name; 
+SELECT athlete.athlete_name 
+FROM athlete, committee, athlete_competition
+WHERE athlete.athlete_id = athlete_competition.athlete_id
+AND athlete_competition.committee_id = committee.committee_id
+AND committee.region = "Kenya"
+ORDER BY athlete.athlete_name; 
+
+SELECT athlete_competition_event.medal, competition.competition_name
+FROM athlete, athlete_competition, competition, athlete_competition_event
+WHERE athlete.athlete_id = athlete_competition.athlete_id 
+AND athlete_competition.committee_id = committee.committee_id
+AND athlete_competition.athlete_competition_id = athlete_competition_event.athlete_competition_id
+AND athlete.athlete_name = "Greg Louganis";
+
+SELECT committee.region, COUNT(athlete_competition_event.medal)
+FROM committee, athlete_competition, athlete_competition_event
+WHERE committee.committee_id = athlete_competition.committee_id 
+AND athlete_competition.athlete_competition_id = athlete_competition_event.athlete_competition_id
+AND medal = "Gold"
+AND ORDER BY COUNT(athlete_competition_event.medal) DESC;
