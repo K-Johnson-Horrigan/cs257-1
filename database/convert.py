480f596c5662338eb6767a00dedaaafdf415612e
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2021-01-23 09:55:11 -0600

    Add python and sql files

---
+++ database/convert.py
@@ -0,0 +1,5 @@
+# Antonia Ritter and Luisa Escosteguy
+# Jan 22, 2021
+# Converts the raw CSV files (athlete_events.csv and noc_regions.csv) 
+# into CSV files matching the tables in database-schema.sql
+
---
+++ database/queries.sql
@@ -0,0 +1,8 @@
+SELECT * 
+FROM olympic_committee
+ORDER BY abbreviation;
+
+SELECT first_name, last_name 
+FROM athlete
+WHERE country = "Kenya"
+ORDER BY last_name; 
