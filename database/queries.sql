87d1062c324502e7f64a792ee136665fcebc4653
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2021-01-24 15:18:40 -0600

    Fix Greg Louganis query, delete convert_old

--- database/queries.sql
+++ database/queries.sql
@@ -9,12 +9,15 @@ AND athlete_competition.committee_id = committee.committee_id
 AND committee.region = 'Kenya'
 ORDER BY athlete.athlete_name; 
 
-SELECT athlete_competition_event.medal, competition.competition_name
-FROM athlete, athlete_competition, competition, athlete_competition_event
+SELECT athlete_competition_event.medal, competition.competition_name, athlete.athlete_name, event.event_name
+FROM athlete, athlete_competition, competition, athlete_competition_event, event
 WHERE athlete.athlete_id = athlete_competition.athlete_id 
 AND athlete_competition.competition_id = competition.competition_id
 AND athlete_competition.athlete_competition_id = athlete_competition_event.athlete_competition_id
-AND athlete.athlete_name = 'Greg Louganis';
+AND athlete_competition_event.event_id = event.event_id
+AND athlete_competition_event.medal IS NOT NULL
+AND athlete.athlete_name LIKE '%"Greg" Louganis'
+ORDER BY competition.competition_name;
 
 SELECT committee.region, COUNT(athlete_competition_event.medal)
 FROM committee, athlete_competition, athlete_competition_event
