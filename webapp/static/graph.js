18eff3f9da8046a125fd8d38b24d28ecdd934e94
K-Johnson-Horrigan <kiara@oneotaslopes.org>
2021-03-15 14:13:58 -0500

    Update graph.js

--- webapp/static/graph.js
+++ webapp/static/graph.js
@@ -9,21 +9,21 @@
 
 
  /**
- * Returns a 2D array (since order matters) of pairs of crop and production 
+ * Returns a 2D array (since order matters) of pairs of crop and production
  * @param  {array} results           an array in format {crop: [year: production, year: production], crop...}
  * @return {array}                   a 2D array with pairs crop and production sorted by production (descending)
- */ 
+ */
 function sortGraphResults(results){
   // sorted results = [[crop, totalProduction], [crop, totalProduction], ...]
   var sortedResults = [];
   // fill sortedResults
   for(var crop in results){
     var totalProduction = 0;
-    var productionExists = false; 
+    var productionExists = false;
     for(var year in results[crop]){
       if(results[crop][year] != null){
         totalProduction += results[crop][year];
-        productionExists = true; 
+        productionExists = true;
       }
     }
     if (productionExists){
@@ -36,82 +36,8 @@ function sortGraphResults(results){
 }
 
 
-/**
- * Creates a table of crop and production and inserts it into the html 
- * @param  {array} sortedResults     a 2D array with pairs crop and production sorted by production (descending)
- */ 
-function makeTotalTable(sortedResults){
-  var html = '<h4>Total Production</h4>'
-            + '<table><thead><tr><th scope="col">Crop</th>'
-            + '<th scope="col">Production (tons)</th></tr></thead><tbody>';
-
-  for(var row in sortedResults){
-    var crop  = sortedResults[row][0];
-    var totalProduction = sortedResults[row][1];
-    //if(totalProduction>0){
-    html += '<tr><th scope="row">' + crop + '</th><td>' + totalProduction.toLocaleString() + '</td>';
-    //}
-  }
-
-  html += '</tbody></table>';
-  var element = document.getElementById('display-table');
-  if (element) {
-    element.innerHTML = html;
-  }
-}
-
-
  /**
- * Creates a line graph of crop production over the years and inserts it into the html 
- * @param  {array} years           an array of years 
- * @param  {array} cropLines       an array of dictionaries where each contains the information for one graph line
- */ 
-function buildGraph(years, cropLines) {
-  // insert graph canvas
-  var element = document.getElementById('display-graph');
-  if (element) {
-    // chartjs graph gets inserted here
-    element.innerHTML = '<canvas id="crop-graph"></canvas>';
-  }
-  
-  var ctx = document.getElementById('crop-graph').getContext('2d');
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-    // The data for our dataset
-    data: {
-        // the x-axis
-        labels: years,
-        // the lines
-        datasets: cropLines
-    },
-    // Configuration options go here
-    options: {
-      hover: {
-        mode: 'nearest',
-        intersect: true
-      },
-      scales: {
-        xAxes: [{
-          display: true,
-          scaleLabel: {display: true, labelString: 'Year'}
-        }],
-        yAxes: [{
-          display: true,
-          scaleLabel: {display: true, labelString: 'Production (tons)'},
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      },
-      legend: {display: true, position: 'right'}
-    }
-  });
-}
-
-
- /**
- * Generates the data and details used to create a graph in buildGraph() 
+ * Generates the data and details used to create a graph in buildGraph()
  * @param  {array} results           an array in format {crop: [year: production, year: production], crop...}
  * @param  {array} sortedResults     a 2D array with pairs crop and production sorted by production (descending)
  */
@@ -119,9 +45,9 @@ function buildGraph(years, cropLines) {
 // results in the form {crop: {year: production, year: production, …}, crop: …}
 function initializeGraph(sortedResults, results) {
 
-  // if there are non-null results 
+  // if there are non-null results
   if (sortedResults.length > 0){
-    
+
     // number of crops to display on graph
     var maxCrops = 8;
     // if it's a one-crop query
@@ -154,17 +80,90 @@ function initializeGraph(sortedResults, results) {
         production = results[crop][year];
         productionData.push(production);
       }
-      // put all the information together for a line 
+      // put all the information together for a line
       var line = {label: crop, backgroundColor: colors[index], borderColor: colors[index], data: productionData, fill: false}
-      // add to list of all lines 
+      // add to list of all lines
       cropLines.push(line)
     }
 
     buildGraph(years, cropLines);
     makeTotalTable(sortedResults);
- 
+
   } else {
-    noResultsMessage(); 
+    noResultsMessage();
   }
 }
 
+
+/**
+* Creates a line graph of crop production over the years and inserts it into the html
+* @param  {array} years           an array of years
+* @param  {array} cropLines       an array of dictionaries where each contains the information for one graph line
+*/
+function buildGraph(years, cropLines) {
+ // insert graph canvas
+ var element = document.getElementById('display-graph');
+ if (element) {
+   // chartjs graph gets inserted here
+   element.innerHTML = '<canvas id="crop-graph"></canvas>';
+ }
+
+ var ctx = document.getElementById('crop-graph').getContext('2d');
+ var chart = new Chart(ctx, {
+   // The type of chart we want to create
+   type: 'line',
+   // The data for our dataset
+   data: {
+       // the x-axis
+       labels: years,
+       // the lines
+       datasets: cropLines
+   },
+   // Configuration options go here
+   options: {
+     hover: {
+       mode: 'nearest',
+       intersect: true
+     },
+     scales: {
+       xAxes: [{
+         display: true,
+         scaleLabel: {display: true, labelString: 'Year'}
+       }],
+       yAxes: [{
+         display: true,
+         scaleLabel: {display: true, labelString: 'Production (tons)'},
+         ticks: {
+           beginAtZero: true
+         }
+       }]
+     },
+     legend: {display: true, position: 'right'}
+   }
+ });
+}
+
+
+/**
+ * Creates a table of crop and production and inserts it into the html
+ * @param  {array} sortedResults     a 2D array with pairs crop and production sorted by production (descending)
+ */
+function makeTotalTable(sortedResults){
+  var html = '<h4>Total Production</h4>'
+            + '<table><thead><tr><th scope="col">Crop</th>'
+            + '<th scope="col">Production (tons)</th></tr></thead><tbody>';
+
+  for(var row in sortedResults){
+    var crop  = sortedResults[row][0];
+    var totalProduction = sortedResults[row][1];
+    //if(totalProduction>0){
+    html += '<tr><th scope="row">' + crop + '</th><td>' + totalProduction.toLocaleString() + '</td>';
+    //}
+  }
+
+  html += '</tbody></table>';
+  var element = document.getElementById('display-table');
+  if (element) {
+    element.innerHTML = html;
+  }
+}
