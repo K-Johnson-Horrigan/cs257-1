5b9a7fa87310ba77b0c11fbac5b565fa214fbf98
K-Johnson-Horrigan <johnsonk4@carleton.edu>
2021-10-03 17:47:07 -0500

    Delete hello directory

--- hello/hello.py
+++
@@ -1,9 +0,0 @@
-# Matthew Rasmussen and Antonia Ritter
-# CS 257 
-# Jan 6 2021 
-
-print("Hello world")
-print("How are you doing? No really, how are you?")
-
-print("What's invisible and smells like bananas?")
-print("Monkey farts")
